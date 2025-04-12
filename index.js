2ce0add1b28528f1c7312f14698e0b25949fc2a1
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-12 11:38:29 -0500

    added fetchcoodrdbyip

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require('../iss_spotter/iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,4 +7,16 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+
+  
+  fetchCoordsByIP(ip, (error, coordinates) => {
+    if (error) {
+      console.log("It didn't work!" , error);
+      return;
+    }
+
+    console.log('It worked! Returned coordinates:' , coordinates);
+
+  });
 });
+
